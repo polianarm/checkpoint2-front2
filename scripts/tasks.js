559559ba652d3b6625d097f3f5d5da3783133494
polianarm <polianariveiro@gmail.com>
2023-04-04 16:16:10 -0300

    inserindo chosebutton

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,45 +1,54 @@
+const closeButtonRef = document.querySelector('#closeApp')
+
 // const token = localStorage.getItem('token')
 
 // function checkToken() {
-
 //     if(token === null) {
-//         window.location.href = '/checkpoint2-front2/signup.html'
-//     }
-    
+//     location.replace('./index.html');
+//  }
 // }
 
 // checkToken()
 
+function logout() {
 
-window.addEventListener('load', function() {
-  
-   
+    location.replace('./index.html');
+  localStorage.clear()
+
+}
+
+
+window.addEventListener('load', function () {
+
+
   const urlUsuario = 'https://todo-api.ctd.academy/v1/users/getMe';
   const token = localStorage.getItem('token');
 
-    obtenerNombreUsuario()
-    // ...
-    
-    function obtenerNombreUsuario() {
-        const settings = {
-          method: 'GET',
-          headers: {
-            authorization: token
-          }
-        };
-        console.log("Consultando mi usuario...");
-        fetch(urlUsuario, settings)
-          .then(response => response.json())
-          .then(data => {
-            console.log("Nombre de usuario:");
-            console.log(data.firstName);
-            const nombreUsuario = document.querySelector('.user-info p');
-            nombreUsuario.innerText = data.firstName;
-          })
-          .catch(error => console.log(error));
+  obtenerNombreUsuario()
+  // ...
+
+  function obtenerNombreUsuario() {
+    const settings = {
+      method: 'GET',
+      headers: {
+        authorization: token
       }
-    
-    
+    };
+    console.log("Consultando mi usuario...");
+    fetch(urlUsuario, settings)
+      .then(response => response.json())
+      .then(data => {
+        console.log("Nombre de usuario:");
+        console.log(data.firstName);
+        const nombreUsuario = document.querySelector('.user-info p');
+        nombreUsuario.innerText = data.firstName;
+      })
+      .catch(error => console.log(error));
+  }
+
+
 
 
 })
+
+closeButtonRef.addEventListener('click', () => logout())
