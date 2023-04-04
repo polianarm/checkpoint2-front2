a7ed8deb5534627a08c780a45e2cd515a429acee
Douglas Queiroz <douglas.lima.queiroz@gmail.com>
2023-04-04 19:55:21 -0300

    signup

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,78 +1,98 @@
-  const urlUsuario = 'https://todo-api.ctd.academy/v1/users/getMe';
-  const token = localStorage.getItem('token');
-  // ----------------------------------------------------------------------------------------------
-                                // CHECAR TOKEN
-  // ----------------------------------------------------------------------------------------------
+const urlUsuario = "https://todo-api.ctd.academy/v1/users/getMe";
+const token = localStorage.getItem("token");
 
-  const tokenCheck = localStorage.getItem('token')
+// ----------------------------------------------------------------------------------------------
+// CHECAR TOKEN
+// ----------------------------------------------------------------------------------------------
 
-  function checkToken() {
-      if(tokenCheck === null) {
-      location.replace('./index.html');
-   }
+const tokenCheck = localStorage.getItem("token");
+
+function checkToken() {
+  if (tokenCheck === null) {
+    location.replace("./index.html");
   }
-  
-  checkToken()
+}
+
+checkToken();
 // ----------------------------------------------------------------------------------------------
-// BOTAO SAIR 
+// BOTAO SAIR
 // ----------------------------------------------------------------------------------------------
 
-const closeButtonRef = document.querySelector('#closeApp')
-  function logout() {
+const closeButtonRef = document.querySelector("#closeApp");
+function logout() {
+  // location.replace("./index.html");
+  // localStorage.clear();
 
-    location.replace('./index.html');
-  localStorage.clear()
+  Swal.fire({
+    title: 'Deseja sair?',
+    icon: 'question',
+    showCancelButton: true,
+    confirmButtonColor: '#7898FF;',
+    cancelButtonColor: '#8E64C5',
+    confirmButtonText: 'Confirmar',
+    cancelButtonText: 'Cancelar'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      // Swal.fire(
+      //   'Até mais',
+      //    'success'
+      // );
+      localStorage.clear();
+      location.replace('./index.html');
+    }
+  });
+
+
 
 }
-  obtenerNombreUsuario()
-  // ...
+
+// ...
 // ----------------------------------------------------------------------------------------------
-                                // Nome de usuario
-  // ----------------------------------------------------------------------------------------------
-  function obtenerNombreUsuario() {
-    const settings = {
-      method: 'GET',
-      headers: {
-        authorization: token
-      }
-    };
-    console.log("Consultando meu usuario...");
-    fetch(urlUsuario, settings)
-      .then(response => response.json())
-      .then(data => {
-        console.log("Nome de usuario:");
-        console.log(data.firstName);
-        const nombreUsuario = document.querySelector('.user-info p');
-        nombreUsuario.innerText = data.firstName;
-      })
-      .catch(error => console.log(error));
-  }
-
-
-
+// Nome de usuario
 // ----------------------------------------------------------------------------------------------
-                                // Criar Novas tarefas POST
-  // ----------------------------------------------------------------------------------------------
-  const urlTarefas = 'https://todo-api.ctd.academy/v1/tasks';
-  function consultarTarefas (){
-
-    const settings = {
-      method: "GET",
-      headers: {
-        authorization: token,
-      },
-    };
-    console.log("Consultar as tarefas");
-
-    fetch(urlTarefas, settings)
+obterNomeUsuario();
+function obterNomeUsuario() {
+  const settings = {
+    method: "GET",
+    headers: {
+      authorization: token,
+    },
+  };
+  console.log("Consultando meu usuario...");
+  fetch(urlUsuario, settings)
     .then((response) => response.json())
-    .then((tareas) => {
-        
+    .then((data) => {
+      console.log("Nome de usuario:");
+      console.log(data.firstName);
+      console.log(data.id);
 
+      const nomeUsuario = document.querySelector(".user-info p");
+      nomeUsuario.innerText = data.firstName;
     })
-  }
+    .catch(() => { 
+     localStorage.clear();
+    location.replace('./index.html');
+  })
+}
 
-  
+// ----------------------------------------------------------------------------------------------
+// Criar Novas tarefas POST
+// ----------------------------------------------------------------------------------------------
+const urlTarefas = "https://todo-api.ctd.academy/v1/tasks";
+function consultarTarefas() {
+  const settings = {
+    method: "GET",
+    headers: {
+      authorization: token,
+    },
+  };
+  console.log("Consultar as tarefas");
+
+  fetch(urlTarefas, settings)
+    .then((response) => response.json())
+    .then((tarefas) => {});
+}
 
 
-closeButtonRef.addEventListener('click', () => logout())
+
+closeButtonRef.addEventListener("click", () => logout());
