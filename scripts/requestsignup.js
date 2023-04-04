068a9fc54e555d7754e131e4de32c6dc58f469ee
polianarm <polianariveiro@gmail.com>
2023-04-04 20:53:47 -0300

    criando-tarefas
    Co-authored-by: Douglas Queiroz <douglasliman@users.noreply.github.com>
    Co-authored-by: Deborah-Borges <Deborah-Borges@users.noreply.github.com>

--- scripts/requestsignup.js
+++ scripts/requestsignup.js
@@ -1,52 +1,52 @@
-const nameInputRef  = document.querySelector('#name');
-const lastNameInputRef  = document.querySelector('#lastName');
-const emailInputRef  = document.querySelector('#email');
-const passwordInputRef  = document.querySelector('#password');
-const passwordCheckInputRef  = document.querySelector('#passwordCheck');
-const submitInputRef  = document.querySelector('#submit');
+// const nameInputRef  = document.querySelector('#name');
+// const lastNameInputRef  = document.querySelector('#lastName');
+// const emailInputRef  = document.querySelector('#email');
+// const passwordInputRef  = document.querySelector('#password');
+// const passwordCheckInputRef  = document.querySelector('#passwordCheck');
+// const submitInputRef  = document.querySelector('#submit');
 
 
 
 
-// Exemplo de Requisição POST
+// // Exemplo de Requisição POST
 
-var userData = {
-    firstName: 'ivin',
-    lastName: 'rodrigues',
-    email: 'ivin3@mail.com.br',
-    password: '1234'
-}
+// var userData = {
+//     firstName: 'ivin',
+//     lastName: 'rodrigues',
+//     email: 'ivin3@mail.com.br',
+//     password: '1234'
+// }
 
-const requestHeaders = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-}
+// const requestHeaders = {
+//     'Accept': 'application/json',
+//     'Content-Type': 'application/json'
+// }
 
-var requestConfig = {
-    method: 'POST',
-    headers: requestHeaders,
-    body: JSON.stringify(userData)
-}
+// var requestConfig = {
+//     method: 'POST',
+//     headers: requestHeaders,
+//     body: JSON.stringify(userData)
+// }
 
-function authUser() {
+// function authUser() {
 
-    fetch('https://todo-api.ctd.academy/v1/users', requestConfig).then(
-    response => {
-        if(response.ok) {
+//     fetch('https://todo-api.ctd.academy/v1/users', requestConfig).then(
+//     response => {
+//         if(response.ok) {
 
-            alert('Você foi cadastrado com sucesso')
+//             alert('Você foi cadastrado com sucesso')
 
-            window.location.href = '/aula-14/login.html'
+//             window.location.href = '/aula-14/login.html'
 
-        } else {
+//         } else {
 
-            alert('O usuário ja foi cadastrado')
+//             alert('O usuário ja foi cadastrado')
 
-        }
-    }
-)
+//         }
+//     }
+// )
     
-}
+// }
 
 
 
--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,5 +1,6 @@
   const urlUsuario = 'https://todo-api.ctd.academy/v1/users/getMe';
   const token = localStorage.getItem('token');
+  const novaTarefa = document.querySelector('#novaTarefa');
   // ----------------------------------------------------------------------------------------------
                                 // CHECAR TOKEN
   // ----------------------------------------------------------------------------------------------
@@ -10,6 +11,8 @@
       if(tokenCheck === null) {
       location.replace('./index.html');
    }
+    
+
   }
   
   checkToken()
@@ -51,28 +54,191 @@ const closeButtonRef = document.querySelector('#closeApp')
 
 
 // ----------------------------------------------------------------------------------------------
-                                // Criar Novas tarefas POST
+                                // Criar Novas tarefa POST
   // ----------------------------------------------------------------------------------------------
-  const urlTarefas = 'https://todo-api.ctd.academy/v1/tasks';
+  const tarefas = 'https://todo-api.ctd.academy/v1/tasks';
   function consultarTarefas (){
 
     const settings = {
-      method: "GET",
-      headers: {
-        authorization: token,
-      },
+      method: 'GET',
+      headers: requestHeaders
     };
     console.log("Consultar as tarefas");
 
-    fetch(urlTarefas, settings)
+    fetch(tarefas, settings)
     .then((response) => response.json())
-    .then((tareas) => {
+    .then((tarefas) => {
+          console.log("Consultar as tarefaas")
+          console.table(tarefas);
         
+  })
+  .catch(error => console.log(error));
+    
+  }
+  // ----------------------------------------------------------------------------------------------
+                                // Criar Novas tarefas POST
+  // ----------------------------------------------------------------------------------------------
 
-    })
+function criarTarefa(event) {
+
+  event.preventDefault()
+// -------------quando criada --------------
+  // {
+  //   "id": 1,
+  //   "description": "Aprender Javascript",
+  //   "completed": false,
+  //   "userId": 1,
+  //   "createdAt": "2021-06-30T22:53:09.549Z"
+  // }
+  // ----------------- OQUE VAI SER AGREGADO------------
+  // {
+  //   "description": "Aprender Javascript",
+  //   "completed": false
+  // }
+  const tarefas = {
+    id: '',
+    description: novaTarefa.value, 
+    completed: false,
+    userId: '',
+    createdAt: ''
   }
 
+  const requestHeaders = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+}
   
+
+  const requestConfig = {
+    method: 'POST',
+    body:JSON.stringify(),
+    headers: requestHeaders,
+}
+  
+fetch(tarefas, requestConfig).then(
+  response => {
+    if(response.ok) {
+      tarefas={
+      tarefas.description
+
+    }
+    }
+  }
+)
+
+}
+
+btnSubmit.addEventListener('click', criarTarefa)
+
 
 
 closeButtonRef.addEventListener('click', () => logout())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------------ COLA 
+
+// // Função que irá criar uma Task
+// function createTask() {
+
+//   // Informações sobre a Task
+//   const taskData = {
+//       description: 'Teste tarefa 2',
+//       completed: true
+//   }
+
+//   // Configuração da Request
+//   var requestConfig = {
+//       method: 'POST',
+//       headers: requestHeaders,
+//       body: JSON.stringify(taskData)
+//   }
+
+//   // Realização da Request para criar uma nova Task
+//   fetch(`${baseUrlAPI}/tasks`, requestConfig).then(
+//       response => {
+//           if(response.ok) {
+//               response.json().then(
+//                   data => {
+//                       console.log(data)
+//                   }
+//               )
+//           }
+//       }
+//   )
+
+// }
+
+
+// // Função que obtém as Tasks criadas pelo usuário logado
+// function getTasks() {
+
+//   // Configurações da Request
+//   var requestConfig = {
+//       method: 'GET',
+//       headers: requestHeaders
+//   }
+
+//   // Requisição para criar a Task
+//   fetch(`${baseUrlAPI}/tasks`, requestConfig).then(
+//       response => {
+//           if(response.ok) {
+//               response.json().then(
+//                   tasks => {
+//                       console.log(tasks)
+//                   }
+//               )
+//           }
+//       }
+//   )
+
+// }
+
+
+
+// // Função que irá obter os dados do usuário Logado
+// function getUserData() {
+
+//   // Configuração da Request
+//   var requestConfig = {
+//       method: 'GET',
+//       headers: requestHeaders
+//   }
+
+//   // Request para obter os Dados do Usuário
+//   fetch(`${baseUrlAPI}/users/getMe`, requestConfig).then(
+//       response => {
+//           if(response.ok) {
+
+//               // Obtém as Tasks do usuário logado
+//               getTasks()
+
+//           } else {
+//               // Verifica se a API retornou o Status code 401(O número 401 significa que o Token fornecido está inválido)
+//               if(response.status === 401) {
+
+//                   // Caso o Token esteja errado será realizar o Logout do usuário na Aplicação
+//                   logout()
+
+//               }
+//           }
+//       }
+//   )
+
+// }
